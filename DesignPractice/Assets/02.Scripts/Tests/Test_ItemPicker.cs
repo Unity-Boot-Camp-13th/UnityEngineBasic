efda50143f3cb4a6e1ddfe25d143ff9e5d151b24
todays-sun-day <178suna@gmail.com>
2025-04-21 17:01:20 +0900

    UI Slot 구현, UI 연습

---
+++ DesignPractice/Assets/02.Scripts/Tests/Test_ItemPicker.cs
@@ -0,0 +1,30 @@
+using DP.Controllers;
+using UnityEngine;
+
+public class Test_ItemPicker : MonoBehaviour
+{
+    Camera _camera;
+
+
+    private void Start()
+    {
+        _camera = Camera.main; // 메인 카메라를 기준으로 마우스 감지를 할 예정
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider.gameObject.TryGetComponent(out ItemController itemController))
+                {
+                    itemController.PickUp();  // 여기서 인벤토리로 이동
+                }
+            }
+        }
+    }
+
+}
